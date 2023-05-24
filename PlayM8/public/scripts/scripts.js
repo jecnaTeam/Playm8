2d5638f5b63d089990e5add2d4cca4ebbf718ce8
D3zyy <michaldolezel@gmail.com>
2023-05-24 09:10:04 +0200

    updated

--- PlayM8/public/scripts/scripts.js
+++ PlayM8/public/scripts/scripts.js
@@ -1,6 +1,20 @@
+
+
 function showDiv() {
     var showDiv = document.getElementById("show");
     var ikonka = document.getElementById("iconn")
     showDiv.style.display = "block";
     ikonka.style.display = "none";
   }
+  function hideDiv() {
+    var showDiv = document.getElementById("show");
+    var ikonka = document.getElementById("iconn")
+    showDiv.style.display = "none";
+    ikonka.style.display = "block";
+  }
+
+
+  document.querySelector("#show").addEventListener("mouseleave", () => {
+    hideDiv();
+    console.log("funguje");
+  });
