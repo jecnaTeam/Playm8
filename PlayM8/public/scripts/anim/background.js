5fdef17ef4abc9927ecde32eb4652fc173497a94
vanis <pavlik.vanis@email.cz>
2023-05-25 09:14:10 +0200

    crads mobile responsivity

--- PlayM8/public/scripts/anim/background.js
+++ PlayM8/public/scripts/anim/background.js
@@ -1,19 +1,4 @@
 
-// Background
-const left = document.getElementById('left');
-const right = document.getElementById('right')
-
-window.addEventListener('scroll', () => {
-    let scroll = window.scrollY; 
-    console.log('before   '+right.style.right);
-    // left.style.top = scroll  + 'px'
-    left.style.left = -scroll * 0.08 + 'px'
-    right.style.right = -scroll * 0.1 + 'px'
-    console.log('after  '+right.style.right);
-    // right.style.transform=`translateX(-${scroll * 0.08 }px)`
-})
-
-
 // Hidden
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -39,7 +24,7 @@ const w4 = document.getElementById('wave-4');
 window.addEventListener('scroll', () => {
     let scroll = window.scrollY; 
 
-    w1.style.left = -scroll * 0.06 + 'px';
+    w1.style.left = -scroll * 0.08 + 'px';
     w2.style.left = -scroll * 0.17 + 'px';
     w3.style.left = -scroll * 0.09 + 'px';
     w4.style.left = -scroll * 0.13 + 'px';
